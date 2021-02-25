05fc11e9c011fef8f46f16d5a2d37dd1580ae076
Owen Gervais <oger99@gmail.com>
2021-02-25 12:10:29 -0500

    second test

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const redisClient = require('./redis-client');
 
 const app = express();
 
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
