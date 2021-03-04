172528b034ba85ed768a683303333980cf556946
Owen Gervais <oger99@gmail.com>
2021-03-04 16:16:37 -0500

    cleaned up current functions for debugging

--- api.js
+++ api.js
@@ -46,7 +46,7 @@ apiRouter.get('/featurestudios', (req, res) => {
 });
 
 
-apiRouter.get('/updateFS', (req, res) => {
+apiRouter.post('/updateFS', (req, res) => {
     console.log('The request method was ')
     console.log(req.method)
     forwardRequestToOnshape(`${onshapeApiUrl}/featurestudios/d/${req.query.documentId}/w/${req.query.workspaceId}/e/${req.query.blockly}`, req, res);
