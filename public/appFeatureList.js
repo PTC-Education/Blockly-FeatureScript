99caebb68a18d7616e404f2803b651f50e0aa9ff
Owen Gervais <oger99@gmail.com>
2021-04-01 11:46:46 -0400

    adding createBlocklyFeature to new file

--- public/appFeatureList.js
+++ public/appFeatureList.js
@@ -14,3 +14,46 @@ async function hasExportFeature(){
     var hasStudio = false
     return hasStudio
   }
+
+
+/*
+   Adds the Blocky custom feature to the Part Studio Feature List 
+
+   Input:
+  
+      namespace: e{elementId}::m{microversionID}, element id and microversion id
+                This is obtained from the GET FeatureStudiosSpecs endpoint 
+
+      sourceMicroversion: The current source microversion of the document
+
+      fsID: This is the element id of the blockly export feature studio
+*/
+
+
+async function createBlocklyFeature(namespace, sourceMicroversion, fSId) {
+  
+    raw = JSON.stringify({
+      "feature": {
+        "type" : 134,
+        "typeName": "BTMFeature",
+        "message" : {
+          "featureType" : "myFeature",
+          "name" : "Blockly Created Feature",
+          "parameters" : [],
+          "namespace" : `${namespace}`
+        }
+      },
+      "serializationVersion": "1.1.20",
+      "sourceMicroversion": `${sourceMicroversion}`
+    })
+   
+    header =  {'Content-Type':'application/json'}
+    
+    try {
+          const response = await fetch(`/api/addFeatureToList${window.location.search}&blockly=${fSId}`, {method: 'POST', body: raw, headers: header})
+          const featurestudios = await response.json();
+          return featurestudios;
+      } catch (error) {
+          console.error(error);
+      }
+  };
